d35bf72048fd1f1d352bba5cf254c7e3f5eecb0d
Maarten L. Hekkelman <maarten@hekkelman.com>
2023-06-27 08:33:42 +0200

    updates

--- src/MSaltApp.cpp
+++ src/MSaltApp.cpp
@@ -59,6 +59,7 @@ std::regex kRecentRE("^" USER HOST PORT "(?:;" USER HOST PORT ";(.+)"
 
 MSaltApp::MSaltApp(MApplicationImpl *inImpl)
 	: MApplication(inImpl)
+	, mIOContext(1)
 	, mConnectionPool(mIOContext)
 {
 	MAcceleratorTable &at = MAcceleratorTable::Instance();
@@ -91,15 +92,15 @@ MSaltApp::~MSaltApp()
 
 int MSaltApp::RunEventLoop()
 {
-	mIOContextThread = std::thread([&io_context = mIOContext]() {
+	mIOContextThread = std::thread([this]() {
 		try
 		{
-			auto wg = asio_ns::make_work_guard(io_context.get_executor());
-			io_context.run();
+			auto wg = asio_ns::make_work_guard(mIOContext.get_executor());
+			auto n = mIOContext.run();
 		}
 		catch (const std::exception &ex)
 		{
-			std::cerr << ex.what() << std::endl;
+			std::cerr << "Exception in io_context thread: " << ex.what() << std::endl;
 		}
 	});
 
